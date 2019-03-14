a1b5fb652e74a708ad2507086eb86c49fef0742c
Tomka <you@example.com>
2019-03-14 12:08:26 +0300

    new

--- untitled2/animals.h
+++ untitled2/animals.h
@@ -1,11 +1,25 @@
+
 #ifndef ANIMALS_H
 #define ANIMALS_H
-
+#include <string>
 
 class animals
 {
+private:
+    const unsigned char go = 0x01;
+    const unsigned char fly = 0x02;
+    const unsigned char swim = 0x03;
+    std::string kind;
+    bool sex;
+    int age;
+    int reprod_age;
+    int reprod_period;
+    int generation;
+
 public:
     animals();
+
+
 };
 
 #endif // ANIMALS_H
